15127abaaf2e3d2325c87d2ee9a336da109b0538
lalalic <lalalic@gmail.com>
2016-05-26 17:59:17 +0800

    publish to npm

--- .travis.deploy.sh
+++
@@ -1,7 +0,0 @@
-#built-in env variables for connection to remote host with ssh, you have to set value in .travis.yml, or travis-ci.org
-#DEPLOY_HOST, DEPLOY_USER, DEPLOY_PASSWORD, TARGET_DEPLOY_FILE[optional, default [DEPLOY_USER's home/__deploy.sh]]
-
-#you can define environment variable to use in this script file,
-#and it will be replaced with value defined in .travis.yml when deploying, such as
-# cd ${git_repository_on_deploy_host} # git_repository_on_deploy_host should be defined in .travis.yml
-# git pull
