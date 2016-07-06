b0f515276fdd6f47d46b0b1c71c10e7175ae24f0
lalalic <lalalic@gmail.com>
2016-07-06 09:54:15 +0800

    columns

--- src/openxml/docx/model/style/section.js
+++ src/openxml/docx/model/style/section.js
@@ -20,6 +20,16 @@ export default class section extends Style.Properties{
 	cols(x){
 		var o=this.asObject(x, parseInt)
 		o.space && (o.space=o.space/20.0)
+		let data=Array.from(x.$('col')).map(a=>{
+			return {
+				width:parseInt(a.attr('w:w'))/20,
+				space:parseInt(a.attr('w:space'))/20
+			}
+		})
+		
+		if(data && data.length)
+			o.data=data
+		
 		return o
 	}
 	static get type(){return 'section'}
