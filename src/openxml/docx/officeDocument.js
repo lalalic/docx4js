574ede4922e7e8cf314c07a59cf8abeea18480d0
lalalic <lalalic@139.com>
2017-04-05 15:29:14 +0800

    legal rel auto apply

--- src/openxml/docx/officeDocument.js
+++ src/openxml/docx/officeDocument.js
@@ -3,17 +3,18 @@ import Part from "../part"
 export class OfficeDocument extends Part{
 	_init(){
 		super._init()
+		const supported="styles,numbering,theme,settings".split(",")
 		this.rels(`Relationship[Target$=".xml"]`).each((i,rel)=>{
 			let $=this.rels(rel)
 			let type=$.attr("Type").split("/").pop()
-			if(type=="customXml")
-				return
-			let target=$.attr("Target")
-			Object.defineProperty(this,type,{
-				get(){
-					return this.getRelObject(target)
-				}
-			})
+			if(supported.indexOf(type)!=-1){
+				let target=$.attr("Target")
+				Object.defineProperty(this,type,{
+					get(){
+						return this.getRelObject(target)
+					}
+				})
+			}
 		})
 	}
 
