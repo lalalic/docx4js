d86fc63672bdcb50d2eda16fe5bdd7ca9833d607
Jean-Baptiste WATENBERG <jeanbaptiste.watenberg@gmail.com>
2020-04-19 18:13:24 +0200

    feat: add absolute url support

--- src/openxml/drawml/index.js
+++ src/openxml/drawml/index.js
@@ -1,94 +1,149 @@
-import Part from "../part"
+import Part from "../part";
 
-export default od=>({
-    filter:":not(a\\:extLst)",
-    id:()=>undefined,
-    ...same("latin,ea,cs".split(","),({attribs:{typeface=""}})=>od.theme.font(typeface)),
+export default (od) => ({
+  filter: ":not(a\\:extLst)",
+  id: () => undefined,
+  ...same("latin,ea,cs".split(","), ({ attribs: { typeface = "" } }) =>
+    od.theme.font(typeface)
+  ),
 
-    ...same("lumMod,lumOff,tint,shade".split(","),({attribs:{val}})=>parseInt(val)/100000),
-    tidy_schemeClr:({val,...effect})=>od.doc.asColor(od.theme.color(val),effect),
-    tidy_srgbClr:({val,...effect})=>od.doc.asColor(val,effect),
-    sysClr:({attribs:{val}})=>val,
-    tidy_solidFill:({color})=>color,
-    rot:v=>parseInt(v)/60000,
+  ...same(
+    "lumMod,lumOff,tint,shade".split(","),
+    ({ attribs: { val } }) => parseInt(val) / 100000
+  ),
+  tidy_schemeClr: ({ val, ...effect }) =>
+    od.doc.asColor(od.theme.color(val), effect),
+  tidy_srgbClr: ({ val, ...effect }) => od.doc.asColor(val, effect),
+  sysClr: ({ attribs: { val } }) => val,
+  tidy_solidFill: ({ color }) => color,
+  rot: (v) => parseInt(v) / 60000,
 
-    blip:n=>{
-        const {attribs:{"r:embed":embed, "r:link":url}}=n
-        if(url)
-            return {url}
-        const part=od.$(n).part()
-        return new Part(part,od.doc).getRel(embed)
-    },
+  blip: (n) => {
+    const {
+      attribs: { "r:embed": embed, "r:link": url },
+    } = n;
+    if (url) return { url };
+    const part = od.$(n).part();
+    return new Part(part, od.doc).getRel(embed);
+  },
 
-    prstGeom(x){
-		return x.attribs.prst
-	},
-	pathLst({children}){
-		const px=x=>od.doc.emu2Px(x)
-        return children.filter(a=>a.name=="a:path")
-            .reduce((d,path)=>{
-                path.children.filter(a=>a.name)
-                    .forEach(a=>{
-                        switch(a.name.split(":").pop()){
-            			case 'moveTo':
-            				d.push('M '+px(a.children[0].attribs.x)+' '+px(a.children[0].attribs.y))
-            				break
-            			case 'lnTo':
-            				d.push('L '+px(a.children[0].attribs.x)+' '+px(a.children[0].attribs.y))
-            				break
-            			break
-            			case 'cubicBezTo':
-            				d.push('L '+px(a.children[0].attribs.x)+' '+px(a.children[0].attribs.y))
-            				d.push('Q '+px(a.children[1].attribs.x)+' '+px(a.children[1].attribs.y)
-            					+' '+px(a.children[2].attribs.x)+' '+px(a.children[2].attribs.y))
-            			break
-            			case 'arcTo':
-            				d.push(`A`)
-            			break
-            			case 'close':
-            				d.push('Z')
-            			break
-            			}
-                    })
-                return d
-            },[]).join(" ")
-	},
-    tidy_custGeom:({pathLst})=>pathLst,
+  prstGeom(x) {
+    return x.attribs.prst;
+  },
+  pathLst({ children }) {
+    const px = (x) => od.doc.emu2Px(x);
+    return children
+      .filter((a) => a.name == "a:path")
+      .reduce((d, path) => {
+        path.children
+          .filter((a) => a.name)
+          .forEach((a) => {
+            switch (a.name.split(":").pop()) {
+              case "moveTo":
+                d.push(
+                  "M " +
+                    px(a.children[0].attribs.x) +
+                    " " +
+                    px(a.children[0].attribs.y)
+                );
+                break;
+              case "lnTo":
+                d.push(
+                  "L " +
+                    px(a.children[0].attribs.x) +
+                    " " +
+                    px(a.children[0].attribs.y)
+                );
+                break;
+                break;
+              case "cubicBezTo":
+                d.push(
+                  "L " +
+                    px(a.children[0].attribs.x) +
+                    " " +
+                    px(a.children[0].attribs.y)
+                );
+                d.push(
+                  "Q " +
+                    px(a.children[1].attribs.x) +
+                    " " +
+                    px(a.children[1].attribs.y) +
+                    " " +
+                    px(a.children[2].attribs.x) +
+                    " " +
+                    px(a.children[2].attribs.y)
+                );
+                break;
+              case "arcTo":
+                d.push(`A`);
+                break;
+              case "close":
+                d.push("Z");
+                break;
+            }
+          });
+        return d;
+      }, [])
+      .join(" ");
+  },
+  tidy_custGeom: ({ pathLst }) => pathLst,
 
-    lvl:v=>parseInt(v),
-    spcPts:({attribs:{val}})=>od.doc.pt2Px(parseInt(val)/100),
-    tidy_spcAft:({spcPts:a})=>a,
-    tidy_spcBef:({spcPts:a})=>a,
+  lvl: (v) => parseInt(v),
+  spcPts: ({ attribs: { val } }) => od.doc.pt2Px(parseInt(val) / 100),
+  tidy_spcAft: ({ spcPts: a }) => a,
+  tidy_spcBef: ({ spcPts: a }) => a,
 
-    buFont:({attribs:{typeface}})=>typeface,
-    buChar:({attribs:{char}})=>char,
-    buSzPts:({attribs:{val}})=>od.doc.pt2Px(parseInt(val)/100),
-    buSzPct:({attribs:{val}})=>parseInt(val)/1000/100,
-    buAutoNum:({attribs})=>({...attribs}),
-    tidy_buClr:({color})=>color,
+  buFont: ({ attribs: { typeface } }) => typeface,
+  buChar: ({ attribs: { char } }) => char,
+  buSzPts: ({ attribs: { val } }) => od.doc.pt2Px(parseInt(val) / 100),
+  buSzPct: ({ attribs: { val } }) => parseInt(val) / 1000 / 100,
+  buAutoNum: ({ attribs }) => ({ ...attribs }),
+  tidy_buClr: ({ color }) => color,
 
-    indent:v=>od.doc.emu2Px(v),
-    marL:v=>od.doc.emu2Px(v),
+  indent: (v) => od.doc.emu2Px(v),
+  marL: (v) => od.doc.emu2Px(v),
 
-    ext:({attribs:{cx,cy}})=>({width:od.doc.emu2Px(cx),height:od.doc.emu2Px(cy)}),
-    off:({attribs:{x,y}})=>({x:od.doc.emu2Px(x),y:od.doc.emu2Px(y)}),
-    tidy_xfrm:({ext={},off={}, ...transform})=>({...ext, ...off, ...transform}),
+  ext: ({ attribs: { cx, cy } }) => ({
+    width: od.doc.emu2Px(cx),
+    height: od.doc.emu2Px(cy),
+  }),
+  off: ({ attribs: { x, y } }) => ({
+    x: od.doc.emu2Px(x),
+    y: od.doc.emu2Px(y),
+  }),
+  tidy_xfrm: ({ ext = {}, off = {}, ...transform }) => ({
+    ...ext,
+    ...off,
+    ...transform,
+  }),
 
-    ...same("ln,lnB,lnR,lnL,lnT,lnTlToBr,lnBlToTr".split(",").map(a=>'tidy_'+a),({w,...props})=>({...props, w:od.doc.emu2Px(w)})),
-    ...same("left,right,top,bottom".split(",").map(a=>'tidy_'+a),({ln})=>ln),
-    tidy_tcTxStyle:({color,...props})=>({...props, solidFill:color}),
+  ...same(
+    "ln,lnB,lnR,lnL,lnT,lnTlToBr,lnBlToTr".split(",").map((a) => "tidy_" + a),
+    ({ w, ...props }) => ({ ...props, w: od.doc.emu2Px(w) })
+  ),
+  ...same(
+    "left,right,top,bottom".split(",").map((a) => "tidy_" + a),
+    ({ ln }) => ln
+  ),
+  tidy_tcTxStyle: ({ color, ...props }) => ({ ...props, solidFill: color }),
 
-    tidy_lnRef:({idx,...ph})=>od.theme.lnRef(idx,ph),
-    tidy_fillRef:({idx,...ph})=>od.theme.fillRef(idx,ph),
-    tidy_EffectRef:({idx,...ph})=>od.theme.effectRef(idx,ph),
-    tidy_fontRef:({idx,...ph})=>od.theme.fontRef(idx,ph),
+  tidy_lnRef: ({ idx, ...ph }) => od.theme.lnRef(idx, ph),
+  tidy_fillRef: ({ idx, ...ph }) => od.theme.fillRef(idx, ph),
+  tidy_EffectRef: ({ idx, ...ph }) => od.theme.effectRef(idx, ph),
+  tidy_fontRef: ({ idx, ...ph }) => od.theme.fontRef(idx, ph),
 
-    names:{
-        schemeClr:"color", srgbClr:"color", sysClr:"color",
-        prstGeom:"geometry", custGeom:"geometry",
-        lnB:"bottom", lnR:"right", lnL:"left", lnT:"top",
-        rot:"rotate",
-    }
-})
+  names: {
+    schemeClr: "color",
+    srgbClr: "color",
+    sysClr: "color",
+    prstGeom: "geometry",
+    custGeom: "geometry",
+    lnB: "bottom",
+    lnR: "right",
+    lnL: "left",
+    lnT: "top",
+    rot: "rotate",
+  },
+});
 
-const same=(keys,fx)=>keys.reduce((fs, k)=>(fs[k]=fx, fs),{})
+const same = (keys, fx) => keys.reduce((fs, k) => ((fs[k] = fx), fs), {});
--- src/openxml/officeDocument.js
+++ src/openxml/officeDocument.js
@@ -1,100 +1,112 @@
-import Part from "./part"
-import drawml from "./drawml"
-export default class extends Part{
-    _init(){
-        super._init(...arguments)
-        this._assignRel(["theme"])
+import Part from "./part";
+import drawml from "./drawml";
+export default class extends Part {
+  _init() {
+    super._init(...arguments);
+    this._assignRel(["theme"]);
 
-        const doc=this.doc
-        const transform=ph=>({
-            ...drawml(this),
-            tidy_schemeClr:({val,...effect})=>{
-                return this.doc.asColor(val=="phClr" ? ph.color :this.theme.color(val),effect)
-            },
-        })
+    const doc = this.doc;
+    const transform = (ph) => ({
+      ...drawml(this),
+      tidy_schemeClr: ({ val, ...effect }) => {
+        return this.doc.asColor(
+          val == "phClr" ? ph.color : this.theme.color(val),
+          effect
+        );
+      },
+    });
 
-        Object.assign(this.theme,{
-            font(typeface){
-                const type={mn:"minor",mj:"major"}
-                const [a,b]=typeface.split(/[+-]/g).filter(a=>a)
-                return this(`a\\:fontScheme>a\\:${type[a]}Font>a\\:${b=="lt"?"latin":b}`).attr("typeface")
-            },
-            color(k){
-                const $=this(`a\\:clrScheme>a\\:${k}`).children().eq(0)
-                return doc.asColor($.attr("lastClr")||$.attr("val"))
-            },
-
-            fillRef(idx,ph){
-                idx=parseInt(idx)
-                if(idx==0 || idx==1000)
-                    return {}
-                if(idx>1000)
-                    return this('a\\:fmtScheme>a\\:bgFillStyleLst')
-                        .children()
-                        .eq(idx-1001)
-                        .props(transform(ph))
-
-                return this('a\\:fmtScheme>a\\:fillStyleLst')
-                    .children()
-                    .eq(idx-1)
-                    .props(transform(ph))
-            },
-
-            lnRef(idx,ph){
-                return this('a\\:fmtScheme>a\\:lnStyleLst')
-                    .children()
-                    .eq(parseInt(idx)-1)
-                    .props(transform(ph))
-            },
-
-            effectRef(idx, ph){
-                return this('a\\:fmtScheme>a\\:effectStyleLst')
-                    .children()
-                    .eq(parseInt(idx)-1)
-                    .children('a\\:effectLst')
-                    .props(transform(ph))
-            },
-
-            fontRef(idx,ph){
-                const $=this('a\\:fmtScheme>a\\:fontScheme>a\\:'+idx+'Font')
-                const latin=$.children('a\\:latin')
-                const ea=$.children('a\\:ea')
-                const cs=$.children('a\\:cs')
-                return {latin:latin.attr("typeface"),ea:ea.attr('typeface'),cs:cs.attr("typeface"),...ph}
-            }
-        })
+    if (!this.theme) {
+      return;
     }
 
-    _assignRel(supported){
-        this.rels(`Relationship[Target$=".xml"]`).each((i,rel)=>{
-			let $=this.rels(rel)
-			let type=$.attr("Type").split("/").pop()
-			if(supported.indexOf(type)!=-1){
-				let target=$.attr("Target")
-				Object.defineProperty(this,type,{
-                    configurable:true,
-					get(){
-						return this.getRelObject(target)
-					}
-				})
-			}
-		})
-    }
+    Object.assign(this.theme, {
+      font(typeface) {
+        const type = { mn: "minor", mj: "major" };
+        const [a, b] = typeface.split(/[+-]/g).filter((a) => a);
+        return this(
+          `a\\:fontScheme>a\\:${type[a]}Font>a\\:${b == "lt" ? "latin" : b}`
+        ).attr("typeface");
+      },
+      color(k) {
+        const $ = this(`a\\:clrScheme>a\\:${k}`).children().eq(0);
+        return doc.asColor($.attr("lastClr") || $.attr("val"));
+      },
 
-    render(createElement, identify=this.constructor.identify.bind(this.constructor)){
+      fillRef(idx, ph) {
+        idx = parseInt(idx);
+        if (idx == 0 || idx == 1000) return {};
+        if (idx > 1000)
+          return this("a\\:fmtScheme>a\\:bgFillStyleLst")
+            .children()
+            .eq(idx - 1001)
+            .props(transform(ph));
 
-    }
+        return this("a\\:fmtScheme>a\\:fillStyleLst")
+          .children()
+          .eq(idx - 1)
+          .props(transform(ph));
+      },
 
-    static identify(wXml, officeDocument){
-        const identities=this.identities
-        const tag=wXml.name.split(":").pop()
-        if(identities[tag])
-            return identities[tag](...arguments)
+      lnRef(idx, ph) {
+        return this("a\\:fmtScheme>a\\:lnStyleLst")
+          .children()
+          .eq(parseInt(idx) - 1)
+          .props(transform(ph));
+      },
 
-        return tag
-    }
+      effectRef(idx, ph) {
+        return this("a\\:fmtScheme>a\\:effectStyleLst")
+          .children()
+          .eq(parseInt(idx) - 1)
+          .children("a\\:effectLst")
+          .props(transform(ph));
+      },
 
-    static identities={
-        
-    }
+      fontRef(idx, ph) {
+        const $ = this("a\\:fmtScheme>a\\:fontScheme>a\\:" + idx + "Font");
+        const latin = $.children("a\\:latin");
+        const ea = $.children("a\\:ea");
+        const cs = $.children("a\\:cs");
+        return {
+          latin: latin.attr("typeface"),
+          ea: ea.attr("typeface"),
+          cs: cs.attr("typeface"),
+          ...ph,
+        };
+      },
+    });
+  }
+
+  _assignRel(supported) {
+    this.rels(`Relationship[Target$=".xml"]`).each((i, rel) => {
+      let $ = this.rels(rel);
+      let type = $.attr("Type").split("/").pop();
+
+      if (supported.indexOf(type) != -1) {
+        let target = $.attr("Target");
+        Object.defineProperty(this, type, {
+          configurable: true,
+          get() {
+            return this.getRelObject(target.replace(/^\//, ""));
+          },
+        });
+      }
+    });
+  }
+
+  render(
+    createElement,
+    identify = this.constructor.identify.bind(this.constructor)
+  ) {}
+
+  static identify(wXml, officeDocument) {
+    const identities = this.identities;
+    const tag = wXml.name.split(":").pop();
+    if (identities[tag]) return identities[tag](...arguments);
+
+    return tag;
+  }
+
+  static identities = {};
 }
